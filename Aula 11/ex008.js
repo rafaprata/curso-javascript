28b698943061f44b2d1b6aaef2b28586dd8778fc
Rafael <rafaelpratasouza@gmail.com>
2021-01-05 07:51:03 -0300

    tentativa de associar o .js ao .html

--- Aula 11/ex008.js
+++ Aula 11/ex008.js
@@ -1,11 +1,11 @@
 //Exemplo de condição simples
 
-var vel = 70
+var vel = ex010.html.window.document.getElementById("textvel")
 
+function calcular(){
 console.log(`A velocidade do carro é ${vel}Km/h`)
-
 if (vel > 60){
     console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
 }
-
 console.log(`Dirija sempre com cuidado.`)
+}

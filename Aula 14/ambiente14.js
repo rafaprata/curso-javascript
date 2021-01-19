52e3d7c8c5de9208552a81b20651d7676d837bf7
Rafael Prata <rafaelprata@pop-os.localdomain>
2021-01-19 17:29:07 -0300

    Aprendendo a utilizar a repetição FOR

---
+++ Aula 14/ambiente14.js
@@ -0,0 +1,19 @@
+/* 
+    Em comparação ao WHILE, temos que:
+    - Inicialização: declaração da Variável
+    - Condição: A mesma condição do WHILE
+    - Incremento: O mesmo incremento indicado no final do bloco de comandos.
+*/
+
+console.log(`Vai começar!`)
+for(var c=1;c<=10;c++){
+    console.log(c)
+}
+
+console.log(`FIM!`)
+/*
+var c = 1
+ while(c <= 10){
+     console.log(`Hey!`)
+     c++
+ }*/

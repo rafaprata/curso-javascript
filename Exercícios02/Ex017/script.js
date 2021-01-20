59b4c74bb57d91229ae1b849b8cf627c6cbb5078
Rafael Prata <rafaelprata@pop-os.localdomain>
2021-01-20 07:44:09 -0300

    Criando a tag option, e mostrando dentro do select o item criado a cada linha da tabuada.

--- Exercícios02/Ex017/script.js
+++ Exercícios02/Ex017/script.js
@@ -1,11 +1,24 @@
 function tabuada(){
     var numtab = window.document.getElementById("txtnum")
-    var n = Number(numtab.value)
-    var r = " "
+    var tab = window.document.getElementById("seltab")
 
-    for(var c = 1; c<=10; c++){
-        r = n * c
-        alert(`${n} x ${c} = ${r}`)
+    if(numtab.value.lenght == 0){
+        window.alert("[ERRO] Por favor, digite um número.")
+    }else{
+        var n = Number(numtab.value)
+        var r = " "
+
+        tab.innerHTML = ` `                                        //Limpa o select antes de mostrar a tabuada.
+
+        for(var c = 1; c<=10; c++){
+            r = n * c
+            var item = document.createElement('option')            //Cria um item com a tag option.
+
+            item.text= `${n} x ${c} = ${r}`
+
+            tab.appendChild(item)                                  //Adiciona o item criado dentro do select no html
+        }
     }
+    
 }
 

3c34dc63fe2e36b217aac9153645286a9ce07888
Rafael Prata <rafaelprata@pop-os.localdomain>
2021-01-20 07:26:00 -0300

    criando as div e montando a tabuada

--- Exercícios02/Ex017/script.js
+++ Exercícios02/Ex017/script.js
@@ -1,1 +1,11 @@
-alert(`Ola`)
+function tabuada(){
+    var numtab = window.document.getElementById("txtnum")
+    var n = Number(numtab.value)
+    var r = " "
+
+    for(var c = 1; c<=10; c++){
+        r = n * c
+        alert(`${n} x ${c} = ${r}`)
+    }
+}
+

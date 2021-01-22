9737a207868421e88f8e29b7ada4550b57d0af8b
Rafael Prata <rafaelprata@pop-os.localdomain>
2021-01-22 11:54:38 -0300

    Adicionando número no array e no select

--- Exercícios03/Ex018/script.js
+++ Exercícios03/Ex018/script.js
@@ -1,5 +1,21 @@
-//Adicionar valor ao select
+//Adicionar número
+var list = []
+
 function adicionar(){
     var txtnum = document.getElementById('txtnum')
+    var tab = document.getElementById('addtab')
+    
+    if(txtnum.value <= 0 || txtnum.value > 100){
+        window.alert('[ERRO] Valor adicionado não permitido!')
+    }else {
+        var num = Number(txtnum.value)
+        list.push(num)
+        var pos = list.indexOf(num)
+        var item = document.createElement('option')
 
+        item.text = `Número ${list[pos]} adicionado`  
+        
+        tab.appendChild(item)
+        txtnum.innerHTML = ' '
+    }
 }
